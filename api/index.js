02b14e75346e91024ebfee07019a5846d96432af
Wesley Cutting <wesleycutting@gmail.com>
2018-06-11 20:17:41 -0600

    Updated API with full CRUD operations

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
-const { send } = require('micro')
+const { send, json } = require('micro')
 // import { send } from 'micro' // Showing an ES6 import below ES5 import
-const { router, get } = require('microrouter')
+const { router, get, post, put, del } = require('microrouter')
 const monk = require('monk')
 
 const url = 'mongodb://heliopeep:passw0rd@ds151530.mlab.com:51530/fs-intro'
@@ -13,10 +13,38 @@ db.then(() => {
 
 const collection = db.get('Books')
 
-const books = async (req, res) => {
+// Create a single Book
+const createBook = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const body = await json(req)
+    const dataToSend = await collection.insert({title: body.title, author: body.author})
+
+    send(res, 200, dataToSend)
+}
+
+// Read all Books
+const getAllBooks = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     const dataToSend = await collection.find({}).then((docs) => docs)
+
+    send(res, 200, dataToSend)
+}
+
+// Update a specific Book
+const updateBook = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const body = await json(req)
+    const dataToSend = await collection.update({ _id: body._id }, { title: body.title, author: body.author })
+
+    send(res, 200, dataToSend)
+}
+
+// Delete a specific book
+const deleteBook = async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const body = await json(req)
+    const dataToSend = await collection.remove({ _id: body._id })
 
     send(res, 200, dataToSend)
 }
@@ -26,7 +54,10 @@ const hello = (req, res) => send(res, 200, `Hello ${req.params.name}`)
 const notfound = (req, res) => send(res, 404, 'Not found route')
 
 module.exports = router(
-    get('/books', books),
+    get('/read-books', getAllBooks),
+    post('/create-book', createBook),
+    put('/update-book', updateBook),
+    del('/delete-book', deleteBook),
     get('/hello/:name', hello),
     get('/*', notfound)
 )
